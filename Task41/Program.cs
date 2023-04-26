4d5be255a4c79d0b5eac1b9ee2f4960cc7094a89
unknown <freekodelka86@gmail.com>
2023-04-26 20:48:31 +0300

    Решена задача 41

---
+++ Task41/Program.cs
@@ -0,0 +1,35 @@
+﻿/*Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.*/
+int size = 10;
+
+int[] EnterNumbers(int size)
+{
+    int[] numbers = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        numbers[i] = InPutNum();
+    }
+    return numbers;
+}
+
+int InPutNum()
+{
+    Console.WriteLine("Enter number: ");
+    return int.Parse(Console.ReadLine());
+}
+
+int CountPositiveNumbers(int[] numbers)
+{
+    int count = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+Console.WriteLine("Количество положительных чисел: " + CountPositiveNumbers(EnterNumbers(size)));
+ 
+ 
